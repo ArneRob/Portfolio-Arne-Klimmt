dc345f4f51100afc919d098ee601d9b2368258bb
ArneRob <arne-klimmt@gmx.de>
2026-04-09 13:15:29 +0200

    added: function to check if input empty, email valid, not finished yet

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
-function startListener() {
+function startContactFormListener() {
     document.querySelector('.contactForm').addEventListener('submit', (e) => e.preventDefault())
+    document.getElementById('privacyCheck').addEventListener('click', togglePrivacyCheckbox)
+    document.getElementById('formNameInput').addEventListener('blur', checkWhichInput)
+    document.getElementById('formEmailInput').addEventListener('blur', checkWhichInput)
+    document.getElementById('formTextInput').addEventListener('blur', checkWhichInput)
 }
 function sendMail() {
     let jsonFormInput = getFormData()
@@ -33,4 +37,65 @@ async function sendFormDataToServer(data) {
     }
 }
 
-startListener()
+function togglePrivacyCheckbox() {
+    document.getElementById('privacyCheck').classList.toggle('contactCheckboxImgChecked')
+    document.getElementById('privacyCheck').classList.toggle('checked')
+}
+
+function checkWhichInput(event) {
+    element = document.getElementById(`${event.currentTarget.id}`)
+    elementId = event.currentTarget.id
+    if (elementId == "formNameInput") {
+        validateInput(element, elementId)
+    } else if (elementId == "formEmailInput") {
+        validateInput(element, elementId)
+
+    } else if (elementId == "formTextInput") {
+        validateInput(element, elementId)
+    }
+    event.currentTarget.id
+}
+
+function validateInput(element, elementId) {
+    if (elementId == "formEmailInput") {
+        if (element.value) {
+            validate(element.value)
+        } else {
+            addInputErrorText(elementId)
+        }
+    } else if (element.value) {
+        removeInputErrorText(elementId)
+        console.log('ok')
+    } else {
+        addInputErrorText(elementId)
+    }
+}
+function removeInputErrorText(elementId) {
+    errorDivId = `error${elementId}`
+    errorElement = document.getElementById(`${errorDivId}`)
+    errorElement.style = ""
+    errorElement.innerHTML = ''
+}
+function addInputErrorText(elementId) {
+    errorDivId = `error${elementId}`
+    errorElement = document.getElementById(`${errorDivId}`)
+    errorElement.style = "color:red"
+    errorElement.innerHTML = 'nischt jeschriebn'
+}
+
+const validateEmail = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+function validate(email) {
+    if (validateEmail(email)) {
+        console.log('true')
+        return true;
+    } else {
+        console.log('false');
+    }
+}
+
+startContactFormListener()
