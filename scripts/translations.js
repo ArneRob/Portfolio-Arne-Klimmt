031422a09df9115436117b4e2416e235b66017fb
ArneRob <arne-klimmt@gmx.de>
2026-04-16 11:46:56 +0200

    translated skillssection

--- scripts/translations.js
+++ scripts/translations.js
@@ -1,7 +1,6 @@
 const translations = {
     EN: {
         "nav.about": "About me",
-        "nav.skills": "Skills",
         "hero.letsTalk": "Lets talk!",
         "hero.scrollDown": "Scroll down!",
         "about.heading": "About Me",
@@ -10,10 +9,15 @@ const translations = {
         "about.location": "Im Living in Halle (Saale), im open to different ways of working, like remotly.",
         "about.project": "In my current position, I have developed a grain storage system software for my company. Although it is not fully finished yet, it already represents a very solid and well-designed project. It helps make storing items easier and allows users to quickly see where everything is located. Instead of using annoying paper notes, it provides a simple and user-friendly interface where users can enter data quickly and easily. You can find the demo in the Portfolio-Section.",
         "about.growth": "I get better with every project I create, and it's really fun to see how much I've grown. I'm now at a point where I feel like I can create anything I want — I just need the time. :)",
+        "skills.lookingFor": "Looking for ",
+        "skills.anotherSkill": "another skill?",
+        "skills.contactSub": "Feel free to contact me, I can bring your idea to life!",
+        "skills.getInTouch": "Get in touch",
+        "skills.description": "In addition to my frontend skills, I also set up my own vServer, dealing with all the challenges that come with it, such as security and other aspects. This gave me insight into what a backend developer does. I really liked it and I can imagine doing more of it in the future.",
+        "skills.continuallyLearning": "Continually learning",
     },
     DE: {
         "nav.about": "Über mich",
-        "nav.skills": "Fähigkeiten",
         "hero.letsTalk": "Lass uns reden!",
         "hero.scrollDown": "Scroll runter!",
         "about.heading": "Über mich",
@@ -22,6 +26,12 @@ const translations = {
         "about.location": "Ich lebe in Halle (Saale) und bin offen für verschiedene Arbeitsweisen, zum Beispiel remote.",
         "about.project": "In meiner aktuellen Position habe ich eine Getreiderlager-Software für mein Unternehmen entwickelt. Obwohl sie noch nicht vollständig fertig ist, stellt sie bereits ein sehr solides und gut gestaltetes Projekt dar. Sie erleichtert die Einlagerung und ermöglicht es, schnell zu sehen, wo sich alles befindet. Anstelle von lästigen Zetteln bietet sie eine einfache und benutzerfreundliche Oberfläche zur schnellen Dateneingabe. Die Demo findest du im Portfolio-Bereich.",
         "about.growth": "Mit jedem Projekt, das ich erstelle, werde ich besser und es macht wirklich Spaß zu sehen, wie weit ich gekommen bin. Ich bin jetzt an einem Punkt, an dem ich das Gefühl habe, alles erschaffen zu können, was ich möchte — ich brauche nur die Zeit. :)",
+        "skills.lookingFor": "Auf der Suche nach ",
+        "skills.anotherSkill": "weiteren Fähigkeit?",
+        "skills.contactSub": "Kontaktiere mich gerne, ich kann deine Idee zum Leben erwecken!",
+        "skills.getInTouch": "Kontakt aufnehmen",
+        "skills.description": "Neben meinen Frontend-Kenntnissen habe ich auch meinen eigenen vServer eingerichtet und mich mit allen damit verbundenen Herausforderungen wie Sicherheit und anderen Aspekten befasst. Das hat mir einen Einblick gegeben, was ein Backend-Entwickler macht. Es hat mir wirklich gut gefallen und ich kann mir vorstellen, in Zukunft mehr davon zu machen.",
+        "skills.continuallyLearning": "Stetig am Lernen",
     }
 }
 
